8bff2f9d21c2e49895c7417db3b5feca59027057
Li Jiaquan <st49306720@gmail.com>
2025-11-20 19:27:09 +0800

    2025/11/20

--- c++_learning/c++_svq_stack/c++_stack.cpp
+++ c++_learning/c++_svq_stack/c++_stack.cpp
@@ -1,33 +1,35 @@
-#include   <iostream>
-#include   <stack>
-using namespace std;
-int main(){
-    int n;
-    cin >>n;
-    int nums[n];
-    
-    while(1){
-        for(int i=0;i<n;i++){
-            cin >> nums[i];
-            if (nums[i]==0){
-                return 0;
+    #include   <iostream>
+    #include   <stack>
+    using namespace std;
+    int main(){
+        int n;
+        cin >>n;
+        int nums[n];
+        
+        while(1){
+            for(int i=0;i<n;i++){
+                cin >> nums[i];
+                if (nums[i]==0){
+                    return 0;
+                }
             }
-        }
-        stack<int> s;
-        int i;
-        for(i=0;i<n;i++){
-            s.push(i+1);
-            while(!s.empty() && s.top()==nums[i]){
-                s.pop();
+            stack<int> s;
+            int i;
+            int rn=0;
+            for(int i=0;i<n;i++){
+                s.push(i+1);
                 
+                while(s.size()&& s.top()==nums[rn]){
+                    s.pop();
+                    rn++;
+                }
+            }
+            if(s.empty()){
+                cout <<"YES"<<endl;
+            }else{
+                cout <<"NO"<<endl;
             }
         }
-        if(s.empty()){
-            cout <<"YES"<<endl;
-        }else{
-            cout <<"NO"<<endl;
-        }
+        
+        return 0;
     }
-    
-    return 0;
-}
